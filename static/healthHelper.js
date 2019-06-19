19f4f2aedbcbc1b3ac6cb720d8840eaf28dbcba7
Ben Cutler <bcutler94@gmail.com>
2019-06-19 07:59:14 -0400

    Fix unit tests

--- static/healthHelper.js
+++ static/healthHelper.js
@@ -20,54 +20,87 @@ function lowCheck(response, arr, message, bool) {
     return [arr, message, bool];
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const lowArray = [];
-    let message = 'Altitude is A-OK';
-    let recover = false;
-    setInterval(() => fetch('http://nestio.space/api/satellite/data')
-        .then(data => data.json())
-        .then(response => {
-            [lowArray, message, recover] = lowCheck(response, lowArray, message, recover);
-            const div = document.getElementById('status');
-            div.innerHTML = message;
-        }), 1000);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     let lowArray = [];
+//     let message = 'Altitude is A-OK';
+//     let recover = false;
+//     setInterval(() => fetch('http://nestio.space/api/satellite/data')
+//         .then(data => data.json())
+//         .then(response => {
+//             [lowArray, message, recover] = lowCheck(response, lowArray, message, recover);
+//             const div = document.getElementById('status');
+//             div.innerHTML = message;
+//         }), 1000);
+// });
 
 // UNIT TESTS
 
-function testLowAltitude() {
-    let start = Date.now();
-    let end = Date.now() + 1000;
-    while (end > start) {
-        const dataPoint = { altitude: 150 + (20 * Math.random()), last_updated: start };
-        lowCheck(dataPoint);
-        start = Date.now();
+let desc = 'Safe altitude test';
+let timeNow = Date.now();
+let timeFuture = Date.now() + 100000;
+let testArr = [{ last_updated: timeNow, altitude: 161 }];
+let testRes = {last_updated: timeFuture, altitude: 175};
+let testMessage = 'Altitude is A-OK';
+let testBool = false;
+let actual = lowCheck(testRes, testArr, testMessage, testBool);
+let expectedArr = [];
+let expectedMessage = 'Altitude is A-OK'
+let expectedBool = false;
+let expected = [expectedArr, expectedMessage, expectedBool];
+assertEqual(actual, expected, desc);
+
+desc = 'Low altitude test';
+timeNow = Date.now();
+timeFuture = Date.now() + 100000;
+testArr = [{ last_updated: timeNow, altitude: 159 }];
+testRes = { last_updated: timeFuture, altitude: 150 };
+testMessage = 'WARNING: RAPID ORBITAL DECAY IMMINENT';
+testBool = false;
+actual = lowCheck(testRes, testArr, testMessage, testBool);
+expectedArr = [{ last_updated: timeNow, altitude: 159 }, { last_updated: timeFuture, altitude: 150 }];
+expectedMessage = 'WARNING: RAPID ORBITAL DECAY IMMINENT'
+expectedBool = true;
+expected = [expectedArr, expectedMessage, expectedBool];
+assertEqual(actual, expected, desc);
+
+
+
+function assertEqual(act, exp, desc) {
+    console.log('------------------');
+    console.log(`** ${desc} **`);
+    let count = 0;
+    if (checkArr(act[0], exp[0]) === false) {
+        console.log('Arrays are not equal');
+        count++;
     }
-    return this.message !== 'Altitude is A-OK';
+    if (act[1] !== exp[1]) {
+        console.log('Incorrect output message');
+        count++;
+    }
+    if (act[2] !== exp[2]) {
+        console.log('Incorrect recovery boolean');
+        count++;
+    }
+    if (count) {
+        console.log(`${count} Failure(s)!!!`);
+    } else {
+        console.log('All test cases passed!!!')
+    }
+    console.log('------------------');
 }
 
-function testLowAltitude () {
-    let start = Date.now();
-    let end = Date.now() + 10000;
-    while (end > start) {
-        const dataPoint = {altitude: 159 - (100 * Math.random()), last_updated: start};
-        lowCheck(dataPoint);
-        start = Date.now();
+function checkObj(a, b) {
+    if (Object.keys(a).length !== Object.keys(b).length) return false;
+    for (const key in a) {
+        if (a[key] !== b[key]) return false;
     }
-    return this.message !== 'WARNING: RAPID ORBITAL DECAY IMMINENT';
+    return true;
 }
 
-function testLowAltitudeChange () {
-    let start = Date.now();
-    let end = Date.now() + 100000;
-    while (end > start) {
-        if (end - 40000 > start) {
-            const dataPoint = { altitude: 159 - (100 * Math.random()), last_updated: start };
-            lowCheck(dataPoint)
-        } else {
-            const dataPoint = { altitude: 180, last_updated: start };
-            lowCheck(dataPoint)
-        } 
+function checkArr(a, b) {
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+        if (!checkObj(a[i], b[i])) return false;
     }
-    return this.message !== 'Sustained Low Earth Orbit Resumed';
+    return true;
 }
